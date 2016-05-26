2190c4b8b4d1999d5585eadef5783a1d960bfb6e
Jaci R <jaci.brunning@gmail.com>
2016-05-26 14:47:16 +0800

    Include kill signal

--- Toast-Core/include/toast/util.hpp
+++ Toast-Core/include/toast/util.hpp
@@ -10,8 +10,9 @@
     #include <time.h>
 #else
     #include <sys/time.h>
-    #include <unistd.h>
     #include <sys/types.h>
+    #include <unistd.h>
+    #include <signal.h>
 #endif
 
 #include <errno.h>
