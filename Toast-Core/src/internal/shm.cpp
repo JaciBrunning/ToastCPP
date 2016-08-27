93873c053e6a42f85bd01e7e903c5a4d282f8785
JacisNonsense <jaci.brunning@gmail.com>
2016-08-27 20:39:38 +0800

    Fix SHM string scope memory leak

--- Toast-Core/src/internal/shm.cpp
+++ Toast-Core/src/internal/shm.cpp
@@ -6,18 +6,19 @@ using namespace std;
 
 SHM_HANDLE SHM::create_shm_file(string name, int size) {
     #ifdef OS_WIN
+	string c_s = "Local\\TOAST_" + name;
         return CreateFileMapping(
             INVALID_HANDLE_VALUE,
             nullptr,
             PAGE_READWRITE,
             0,
             size,
-            ("Local\\TOAST_" + name).c_str()
+	    (char *)c_s.c_str()
         );
     #else
-        char *file = (char *)("/TOAST_" + name).c_str();
+	string c_s = "/TOAST_" + name;
         return shm_open(
-            file,
+            (char *)c_s.c_str(),
             O_RDWR | O_CREAT,
             S_IRUSR | S_IWUSR
         );
@@ -26,15 +27,16 @@ SHM_HANDLE SHM::create_shm_file(string name, int size) {
 
 SHM_HANDLE SHM::open_shm_file(string name) {
     #ifdef OS_WIN
+	string c_s = "Local\\TOAST_" + name;
         return OpenFileMapping(
             FILE_MAP_ALL_ACCESS,
             FALSE,
-            ("Local\\TOAST_" + name).c_str()
+	    (char *)c_s.c_str()
         );
     #else
-        char *file = (char *)("/TOAST_" + name).c_str();
-        return shm_open(
-            file,
+        string c_s = "/TOAST_" + name;
+	return shm_open(
+            (char *)c_s.c_str(),
             O_RDWR | O_CREAT,
             S_IRUSR | S_IWUSR
         );
@@ -64,7 +66,7 @@ void SHM::close_shm_file(string name, SHM_HANDLE handle) {
     #ifdef OS_WIN
         CloseHandle(handle);
     #else
-        char *file = (char *)("/TOAST_" + name).c_str();
-        shm_unlink(file);
+	string c_s = "/TOAST_" + name;
+        shm_unlink((char *)c_s.c_str());
     #endif
 }
